58e3a764f19af01a133bfb3af15fb51e96efdf3c
unknow <138153020+ff6f8d68@users.noreply.github.com>
2024-08-23 08:37:06 -0700

    Update IPNMT2.sh

--- IPNMT2.sh
+++ IPNMT2.sh
@@ -36,14 +36,16 @@ display_help() {
 
 # Function to list network interfaces with IP addresses and MAC addresses
 list_network_info() {
-    printf '%10s %32s %32s\n' interface ipaddress macaddress
-    printf '%s\n' '----------------------------------------------------------------------------'
-    for each in $(ip address | grep -oP '(^[\d]+:\s)\K[\d\w]+'); do
-        mac=$(ip address show ${each} | grep -oP '(?<=link/ether\s)\K[\da-f:]+|(?<=link/loopback\s)\K[\da-f:]+')
-        for address in $(ip address show ${each} | grep -oP '(?<=inet\s)\K[\d.]+|(?<=inet6\s)\K[\da-f:]+'); do
-            printf '%10s %32s %32s\n' ${each} ${address} ${mac}
-        done
-    done
+ #!/bin/bash
+
+printf '%10s %32s %32s\n' interface ipaddress macaddress
+printf '%s\n' '----------------------------------------------------------------------------'
+for each in $(ip address | grep -oP '(^[\d]+:\s)\K[\d\w]+'); do
+  mac=$(ip address show ${each} | grep -oP '(?<=link/ether\s)\K[\da-f:]+|(?<=link/loopback\s)\K[\da-f:]+')
+  for address in $(ip address show ${each} | grep -oP '(?<=inet\s)\K[\d.]+|(?<=inet6\s)\K[\da-f:]+'); do
+    printf '%10s %32s %32s\n' ${each} ${address} ${mac}
+  done
+done
 }
 
 # Function to scan the network for devices
